5c7ca56373e3ff0b00aef616be140e5b5ecad30d
Usman Chaudhary <uzmani@yahoo.com>
2017-12-18 12:08:23 -0800

    debbug env

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 // key.js - figure out what set of credentials to return
-
+console.log('process.env.NODE_ENV: ', process.env.NODE_ENV);
 if (process.env.NODE_ENV === 'production') {
   // We are in production - return prod keys
   module.exports = require('./prod');
